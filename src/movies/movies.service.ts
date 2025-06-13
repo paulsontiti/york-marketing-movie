42b9cf11d29e3597a925b60feada9c9e611af15b
Nwoha Paulson <paulsontiti@gmail.com>
2025-06-13 20:19:32 +0100

    prisma

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Movie } from "../../generated/prisma";
+import { Movie } from '@prisma/client';
+
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,6 +1,7 @@
 
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from "../../generated/prisma"
+
+import {PrismaClient} from "@prisma/client"
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
