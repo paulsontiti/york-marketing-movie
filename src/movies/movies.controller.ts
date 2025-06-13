b64c6b87697270fa59a0c69d9b114e0206f0e49b
Nwoha Paulson <paulsontiti@gmail.com>
2025-06-13 20:21:08 +0100

    prisma

--- src/movies/movies.controller.ts
+++ src/movies/movies.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { MoviesService } from './movies.service';
-import { Movie } from '@prisma/client';
 
 
 @Controller('movies')
@@ -8,13 +7,13 @@ export class MoviesController {
     constructor(private readonly moviesService: MoviesService) {}
 
     @Get()
-    async getMovies():Promise<Movie[]>{
+    async getMovies():Promise<any[]>{
 
       return this.moviesService.getMovies();
     }
 
     @Get(":random")
-    async gettenRandomMovies(@Param("random") random:string):Promise<Movie[]>{
+    async gettenRandomMovies(@Param("random") random:string):Promise<any[]>{
 
       return this.moviesService.getRandomMovies(random);
     }
